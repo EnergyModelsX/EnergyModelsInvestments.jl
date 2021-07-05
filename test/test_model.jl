3ad553057b25805dc39f188d018495e11ef51f75
Lars Hellemo <Lars.Hellemo@sintef.no>
2021-07-05 17:11:33 +0200

    Skeleton for test of investment model

---
+++ test/test_model.jl
@@ -0,0 +1,16 @@
+
+@testset "Investment Model" begin
+    
+    # Create simple model
+    
+    # Dummy data
+    
+    # Dummy for now
+    @test 1 == 1
+    
+
+    # Check model
+
+    # Check results
+
+end
