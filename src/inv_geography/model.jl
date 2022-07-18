c9e8e37cc8fd6564ec735982a32d0fd2f121ccaa
Julian Straus <Julian.Straus@sintef.no>
2022-07-18 10:31:14 +0200

    Updated geography test - Included new tests for ContinuousInvestment, SemiContinuousInvestment,   and IntegerInvestment - Excluded new tests for DiscreteInvestment and FixedInvestment due to   errors associated with cm.Trans_cap as TimeProfile   --> These bugs need to be fixed. It is a philosophical questions to be       discussed

--- src/inv_geography/model.jl
+++ src/inv_geography/model.jl
@@ -122,7 +122,7 @@ function constraints_transmission_invest(m, 𝒯, ℒᵗʳᵃⁿˢ)
     # Transmission capacity updating
     for l ∈ ℒᵗʳᵃⁿˢᴵⁿᵛ, cm ∈ corridor_modes_with_inv(l)
         for t_inv ∈ 𝒯ᴵⁿᵛ
-            start_cap= get_start_cap(cm, t_inv, l.Data["InvestmentModels"][cm].Trans_start)
+            start_cap = get_start_cap(cm, t_inv, l.Data["InvestmentModels"][cm].Trans_start)
             @constraint(m, m[:trans_cap_current][l, t_inv, cm] <=
                                 l.Data["InvestmentModels"][cm].Trans_max_inst[t_inv])
             @constraint(m, m[:trans_cap_current][l, t_inv, cm] ==
@@ -143,6 +143,7 @@ function get_start_cap(cm::GEO.TransmissionMode, t, ::Nothing)
         print("Type error of cm.Trans_cap")
     end
 end
+
 """
     set_trans_cap_installation(m, l, 𝒯ᴵⁿᵛ, cm, investmentmode)
 
@@ -191,7 +192,6 @@ function set_trans_cap_installation(m, l, 𝒯ᴵⁿᵛ, cm, ::SemiContinuousInv
                             * m[:trans_invest_b][l, t_inv, cm]) 
         @constraint(m, m[:trans_cap_rem][l, t_inv, cm] == 0)
     end
-    println("Function 4 is used.")
 end
 
 function set_trans_cap_installation(m, l, 𝒯ᴵⁿᵛ, cm, ::FixedInvestment)
