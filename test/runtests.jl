561ecacc5d81c1a43542d4727b5e8c6ff1f91a46
Julian Straus <julian.straus@sintef.no>
2024-03-06 10:28:01 +0100

    Add example test

--- test/runtests.jl
+++ test/runtests.jl
@@ -18,7 +18,7 @@ include("utils.jl")
     # include("test_model.jl")
     include("test_lifetime.jl")
     # include("test_checks.jl")
-    # include("test_examples.jl")
+    include("test_examples.jl")
 
     @testset "w/Geography" begin
         include("test_geo.jl")
