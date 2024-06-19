c7f18265f41ad528a6b931119e9cf0e1433744e0
Lars Hellemo <Lars.Hellemo@sintef.no>
2024-06-19 13:59:04 +0200

    Move GeoExt to EMG

--- ext/EMIGeoExt/utils.jl
+++
@@ -1,7 +0,0 @@
-"""
-    EMI.get_var_inst(m, prefix::Symbol, tm::EMG.TransmissionMode)
-
-When the transmission mode `tm` is used as conditional input, it extracts only the variable
-for the specified transmission mode.
-"""
-EMI.get_var_inst(m, prefix::Symbol, tm::EMG.TransmissionMode)  = m[Symbol(prefix)][tm, :]
