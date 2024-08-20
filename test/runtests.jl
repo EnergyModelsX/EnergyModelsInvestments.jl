e662c81c4649e9cb4e74e73916e70dbe9864002e
Julian Straus <julian.straus@sintef.no>
2024-08-20 10:42:16 +0200

    Included tests again

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,32 +3,32 @@ using JuMP
 using Pkg
 using Test
 
-# using EnergyModelsBase
-# using EnergyModelsInvestments
-# using TimeStruct
+using EnergyModelsBase
+using EnergyModelsInvestments
+using TimeStruct
 
-# const EMB = EnergyModelsBase
-# const EMI = EnergyModelsInvestments
-# const TS = TimeStruct
+const EMB = EnergyModelsBase
+const EMI = EnergyModelsInvestments
+const TS = TimeStruct
 
-# include("utils.jl")
+include("utils.jl")
 
-# @testset "Investments" begin
-#     redirect_stdio(stdout=devnull) do
-#         @testset "Investments | model" begin
-#             include("test_model.jl")
-#         end
+@testset "Investments" begin
+    redirect_stdio(stdout=devnull) do
+        @testset "Investments | model" begin
+            include("test_model.jl")
+        end
 
-#         @testset "Investments | lifetime" begin
-#             include("test_lifetime.jl")
-#         end
+        @testset "Investments | lifetime" begin
+            include("test_lifetime.jl")
+        end
 
-#         @testset "Investments | checks" begin
-#             include("test_checks.jl")
-#         end
+        @testset "Investments | checks" begin
+            include("test_checks.jl")
+        end
 
-#         @testset "Investments | examples" begin
-#             include("test_examples.jl")
-#         end
-#     end
-# end
+        @testset "Investments | examples" begin
+            include("test_examples.jl")
+        end
+    end
+end
